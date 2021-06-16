3a714241f992f101873d807eb1abc886be4ed597
AtharvNadkarni2013 <atharvmnadkarni@gmail.com>
2021-06-16 18:55:02 +0530

    Added Bot files and token

---
+++ main.js
@@ -0,0 +1,42 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const prefix = "+";
+const fs = require("fs");
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync("./commands/").filter(file => file.endsWith(".js"));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+const token = require("token.txt");
+const tokenContent = fs.readFileSync(token);
+
+client.on("ready", () => {
+    console.log("BFG is now online!!");
+    client.user.setActivity("Being Used In " + client.guilds.cache.size + " Servers | +help", {
+        type: "PLAYING"
+    }).catch(console.error);
+});
+client.on("message", message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+    if (command === "ping") {
+        client.commands.get("ping").execute(message, args);
+    } else if (command === "cheenta") {
+        client.commands.get("cheenta").execute(message, args);
+    } else if (command === "youtube") {
+        client.commands.get("youtube").execute(message, args);
+    } else if (command === "whoisatharv") {
+        client.commands.get("whoisatharv").execute(message, args);
+    } else if (command === "help") {
+        client.commands.get("help").execute(message, args, Discord);
+    } else if (command === "clear") {
+        client.commands.get("clear").execute(message, args);
+    } else if (command === "kick") {
+        client.commands.get("kick").execute(message, args);
+    } else if (command === "mute") {
+        client.commands.get("mute").execute(message, args);
+    }
+});
+client.login(`${tokenContent}`);
